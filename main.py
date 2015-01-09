6cf7445ad2940c6334b93a5a863ca30e34404036
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2015-01-10 03:17:46 +0700

    Fix main eternal loop. Add debug infos.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import logging.handlers
 import os
 
 import modem
+from gsmmodem.exceptions import TimeoutException
 
 LOG_FILENAME = './log/runningtext.log'
 
@@ -41,9 +42,9 @@ def init_logger():
 
 def handle_sms(sms):
 	logger = logging.getLogger(__name__)
-	logger.info(u'== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}'.format(sms.number, sms.time, sms.text))
+	logger.info('== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}'.format(sms.number, sms.time, sms.text))
 	logger.info('Replying to SMS...')
-	sms.reply(u'SMS received: "{0}{1}"'.format(sms.text[:20], '...' if len(sms.text) > 20 else ''))
+	sms.reply('SMS received: "{0}{1}"'.format(sms.text[:20], '...' if len(sms.text) > 20 else ''))
 	logger.info('SMS sent')
 
 def main():
@@ -53,15 +54,37 @@ def main():
 	config = ConfigParser.SafeConfigParser()
 	config.read('app.cfg')
 	modem_port = config.get('modem', 'port')
+	
 	m = modem.GsmModem(modem_port, smsReceivedCallbackFunc=handle_sms)
 	m.connect()
-	logger.info('Connected GsmModem at port ' + modem_port)
-	logger.info('Modem mfg: ' + m.manufacturer + ' Model: ' + m.model)
+	logger.info('Connected GsmModem at port {0}'.format(modem_port))
+	logger.info('Modem mfg: {0} Model: {1}'.format(m.manufacturer, m.model))
+
+	logger.info('Waiting for network...')
+	while True:
+		try:
+			m.waitForNetworkCoverage(5)
+		except TimeoutException:
+			logger.info('No network'.format(modem_port))
+			continue
+		else:
+			break
+	logger.info('Network found: {0}'.format(m.networkName))
+	logger.info('Signal strength: {0}'.format(m.signalStrength))
+	
+	m.smsTextMode = True
+	stored_sms = m.listStoredSms(delete=True)
+	logger.info('Deleted {0} stored messages'.format(len(stored_sms)))
+
 	print('Listening...')
 	try:	
 		while True:
 			pass
+	except KeyboardInterrupt:
+		pass
 	finally:
+		print('Closing...')
+		logger.info('Closing GsmModem...')
 		m.close()
 		logger.info('Closed GsmModem')
 	
