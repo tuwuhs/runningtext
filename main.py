e80eb2957077cc8a1f6ac8267061fb89d2566dee
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2015-01-15 07:35:20 +0700

    Fix logging. Refactor to SmsServer class

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import errno
 import logging
 import logging.handlers
 import os
+import time
 
-import modem
-from gsmmodem.exceptions import TimeoutException
+import smsserver
 
 LOG_FILENAME = './log/runningtext.log'
 
@@ -34,47 +34,21 @@ def init_logger():
 	handler = MyRotatingFileHandler(LOG_FILENAME, maxBytes=1000000, backupCount=10)
 	handler.setFormatter(formatter)
 	
-	logger = logging.getLogger(__name__) 
+	logger = logging.getLogger() 
 	logger.setLevel(logging.INFO)
 	logger.addHandler(handler)
 	
 	logger.info('Logger initialized')
 
-def handle_sms(sms):
-	logger = logging.getLogger(__name__)
-	logger.info('== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}'.format(sms.number, sms.time, sms.text))
-	logger.info('Replying to SMS...')
-	sms.reply('SMS received: "{0}{1}"'.format(sms.text[:20], '...' if len(sms.text) > 20 else ''))
-	logger.info('SMS sent')
-
 def main():
 	init_logger()
-	logger = logging.getLogger(__name__)
 
 	config = ConfigParser.SafeConfigParser()
 	config.read('app.cfg')
 	modem_port = config.get('modem', 'port')
 	
-	m = modem.GsmModem(modem_port, smsReceivedCallbackFunc=handle_sms)
-	m.connect()
-	logger.info('Connected GsmModem at port {0}'.format(modem_port))
-	logger.info('Modem mfg: {0} Model: {1}'.format(m.manufacturer, m.model))
-
-	logger.info('Waiting for network...')
-	while True:
-		try:
-			m.waitForNetworkCoverage(5)
-		except TimeoutException:
-			logger.info('No network'.format(modem_port))
-			continue
-		else:
-			break
-	logger.info('Network found: {0}'.format(m.networkName))
-	logger.info('Signal strength: {0}'.format(m.signalStrength))
-	
-	m.smsTextMode = True
-	stored_sms = m.listStoredSms(delete=True)
-	logger.info('Deleted {0} stored messages'.format(len(stored_sms)))
+	sms_server = smsserver.SmsServer(modem_port)
+	sms_server.connect()
 
 	print('Listening...')
 	try:	
@@ -84,9 +58,7 @@ def main():
 		pass
 	finally:
 		print('Closing...')
-		logger.info('Closing GsmModem...')
-		m.close()
-		logger.info('Closed GsmModem')
+		sms_server.disconnect()
 	
 if __name__ == '__main__':
 	main()
---
+++ smsserver.py
@@ -0,0 +1,48 @@
+
+import logging
+
+import modem
+import commandserver
+from gsmmodem.exceptions import TimeoutException
+
+logger = logging.getLogger(__name__)
+
+class SmsServer(object):
+	def __init__(self, modem_port):
+		self._is_connected = False
+		self._modem = modem.GsmModem(modem_port, smsReceivedCallbackFunc=self.handle_sms)
+		pass
+	
+	def connect(self):
+		logger.info('Connecting GsmModem at port {0}...'.format(self._modem.port))
+		self._modem.connect()		
+		logger.info('Connected GsmModem at port {0}'.format(self._modem.port))
+		logger.info('Modem mfg: {0} Model: {1}'.format(self._modem.manufacturer, self._modem.model))
+		logger.info('Waiting for network...')
+		while True:
+			try:
+				self._modem.waitForNetworkCoverage(5)
+			except TimeoutException:
+				logger.info('No network')
+				continue
+			else:
+				break
+		logger.info('Network found: {0}'.format(self._modem.networkName))
+		logger.info('Signal strength: {0}'.format(self._modem.signalStrength))
+		
+		self._modem.smsTextMode = True
+		stored_sms = self._modem.listStoredSms(delete=True)
+		logger.info('Deleted {0} stored messages'.format(len(stored_sms)))
+	
+	def disconnect(self):
+		logger.info('Closing GsmModem at port {0}...'.format(self._modem.port))
+		self._modem.close()
+		logger.info('Closed GsmModem at port {0}'.format(self._modem.port))
+	
+	def handle_sms(self, sms):
+		logger.info('== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}'.format(sms.number, sms.time, sms.text))
+		logger.info('Replying to SMS...')
+		sms.reply('SMS received: "{0}{1}"'.format(sms.text[:20], '...' if len(sms.text) > 20 else ''))
+		logger.info('SMS sent')
+		
+	
